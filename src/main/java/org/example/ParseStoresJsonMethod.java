6f5099efa9a298e64d36b1e1827e367468b1acc3
Shaaldy <shaldeeman@gmail.com>
2023-12-06 18:26:33 +0500

    Парс через Json

--- src/main/java/org/example/ParseStoresJsonMethod.java
+++ src/main/java/org/example/ParseStoresJsonMethod.java
@@ -2,104 +2,61 @@ package org.example;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseStoresJsonMethod {
-
     public static void main(String[] args) throws IOException {
         Parse5ka parse5ka = new Parse5ka();
-        parse5ka.parse();
+        String par = parse5ka.parse();
+        parse5ka.makeItem(par);
     }
 }
 
-class ParseWB {
-    String url;
-    int brandId;
-
-    ParseWB() {
-        this.url = "https://catalog.wb.ru/brands/m/catalog?TestGroup=rec_search_goods_new_model&TestID=370&appType=1&brand=27445&curr=rub&dest=-1113021&sort=popular&spp=28";
-        this.brandId = GetBrandId(url);
-    }
-
-    static int GetBrandId(String url) {
-        String regex = "&brand=(\\d+)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(url);
-
-        if (matcher.find()) {
-            String brandId = matcher.group(1);
-            return Integer.parseInt(brandId);
-        }
-        return -1;
-    }
-
-    void parse() throws IOException {
-        Connection.Response response = Jsoup.connect(String.format("https://catalog.wb.ru/brands/m/catalog?TestGroup=rec_search_goods_new_model&TestID=370&appType=1&brand=%s&curr=rub&dest=-1113021&sort=popular&spp=28", brandId))
-                .header("Accept", "*/*")
-                .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("Connection", "keep-alive")
-                .header("Origin", "https://www.wildberries.ru")
-                .header("Referer", "https://www.wildberries.ru/brands/msi")
-                .header("Sec-Fetch-Dest", "empty")
-                .header("Sec-Fetch-Mode", "cors")
-                .header("Sec-Fetch-Site", "cross-site")
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0")
-                .header("sec-ch-ua", "\"Chromium\";v=\"118\", \"Opera GX\";v=\"104\", \"Not=A?Brand\";v=\"99\"")
-                .header("sec-ch-ua-mobile", "?0")
-                .header("sec-ch-ua-platform", "\"Windows\"")
-                .method(org.jsoup.Connection.Method.GET)
-                .ignoreContentType(true)
-                .execute();
-        System.out.println(response.parse());
-        System.out.println(response.statusCode());
-    }
-}
-
-
-class Parse5ka{
-    Parse5ka(){
-        this.url = "https://skidkaonline.ru/apiv3/products/?limit=310&offset=0&shop_id=9&city_id=49&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today";
+class Parse5ka {
+    Parse5ka() {
+        String url = "https://skidkaonline.ru/apiv3/products/?limit=310&offset=0&shop_id=9&city_id=49&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today";
         this.shopId = getShopId(url);
         this.city_id = getCityId(url);
     }
-    String url;
-    int shopId;
-    int city_id;
 
+    private final int shopId;
+    private final int city_id;
 
-    static int getShopId(String url){
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    static int getShopId(String url) {
         String regex = "&shop_id=(\\d+)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
-        if (matcher.find()){
+        if (matcher.find()) {
             String shop_id = matcher.group(1);
             return Integer.parseInt(shop_id);
         }
         return -1;
     }
 
-    static int getCityId(String url){
+    static int getCityId(String url) {
         String regex = "&city_id=(\\d+)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
-        if (matcher.find()){
+        if (matcher.find()) {
             String city_id = matcher.group(1);
             return Integer.parseInt(city_id);
         }
         return -1;
     }
 
-   public void parse() throws  IOException{
+    String parse() throws IOException {
 
-        Connection.Response response = Jsoup.connect(String.format("https://skidkaonline.ru/apiv3/products/?limit=10&offset=0&shop_id=%s&city_id=%s&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today", shopId, city_id))
+        Connection.Response response = Jsoup.connect(String.format("https://skidkaonline.ru/apiv3/products/?limit=206   а   &offset=0&shop_id=%s&city_id=%s&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today", shopId, city_id))
                 .header("Accept", "*/*")
                 .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
                 .header("Connection", "keep-alive")
@@ -128,24 +85,28 @@ class Parse5ka{
                 .ignoreContentType(true)
                 .execute();
 
-        System.out.println(response.parse());
-
-   }
-
-    void createCSV() throws IOException {
-        CsvMapper mapper = new CsvMapper();
-        CsvSchema schema = mapper.schemaFor(Parse5ka.class)
-                .withColumnSeparator(';')
-                .withoutQuoteChar()
-                .withHeader();
-        ObjectWriter writer = mapper.writer(schema);
-        //writer.writeValue(new FileWriter("wb_data.csv", StandardCharsets.UTF_8));
+        return String.valueOf(response.parse().text());
     }
 
+    public void makeItem(String resParse) throws JsonProcessingException {
+        Items items = objectMapper.readValue(resParse, Items.class);
+        List<Item> itemList = items.getProducts();
+        for(Item item: itemList){
+            String id = item.getId();
+            String name = item.getName();
+            String pricebefore = item.getPricebefore();
+            String priceafter = item.getPriceafter();
+            String startdate = item.getStartdate();
+            String enddate = item.getEnddate();
+            System.out.println("Id: " + id + '\n' + "Name: " + name + '\n' + "Цена до " + pricebefore + '\n' + "Цена после " + priceafter + '\n' + "Даты - "  + startdate + " - " + enddate);
+        }
+    }
 
 }
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Item{
+class Item {
+
     @JsonProperty("id")
     private String id;
     @JsonProperty("pricebefore")
@@ -159,26 +120,36 @@ class Item{
     @JsonProperty("name")
     private String name;
 
-    public String getId(){
+    public String getId() {
         return this.id;
     }
-    public String getPricebefore(){
+
+    public String getPricebefore() {
         return this.pricebefore;
     }
 
-    public String getPriceafter(){
+    public String getPriceafter() {
         return this.priceafter;
     }
-    public String getStartdate(){
+
+    public String getStartdate() {
         return this.startdate;
     }
 
-    public String getEnddate(){
+    public String getEnddate() {
         return this.enddate;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
+}
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Items{
+    @JsonProperty("products")
+    private List<Item> products;
+    public List<Item> getProducts() {
+        return products;
+    }
 }
